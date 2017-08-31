e2581a4d0fcaea08904c49ffd0150d4a6a13fefc
developerjaym <jaymansmann@yahoo.com>
2017-08-31 17:02:44 -0500

    finished functional shopping cart

--- lib.js
+++ lib.js
@@ -30,27 +30,27 @@ const calculateTotals =
           //get the listing's price and add it to a running total
       return carts.reduce(//turns my carts array into an array of {customer, total} (this is the desired output)
         //callback function
-        (previouslyReturned, currentElement) => //currentElement is a cart
+        (arrayToReturn, currentCart) => //currentElement is a cart
         {
-          const lovelyTotal = currentElement.items.reduce(//this should return a lovely total of everything in the cart
-            (prev, cur) =>//cur will be an item from the currentElement cart, prev will be the running total
+          const lovelyTotal = currentCart.items.reduce(//this should return a lovely total of everything in the cart
+            (runningTotal, currentItem) =>//cur will be an item from the currentElement cart, prev will be the running total
             {
               //now I need to go through each listing and add its price to my total
                 listings.reduce//should return false if the item is not there, otherwise it just runs through and does some totalling
                 (//this reduce function doesn't actually return anything
-                  (p, c) =>//c is a listing (name, price)
+                  (whetherFailed, currentListing) =>//c is a listing (name, price)
                   {
-                    prev = prev + listedPrice(c)(cur)//listedPrice(listing)(item) returns the price of that item
+                    runningTotal = runningTotal + listedPrice(currentListing)(currentItem)//listedPrice(listing)(item) returns the price of that item
                   },
                   false//start as false, if it's not in there, it will return false (this should not happen)
                 )
-                return prev;//the running total
+                return runningTotal;//the running total
               }
               ,0//start the running total at 0
           )
           //get name
-          previouslyReturned.push({customer: currentElement.customer, total: lovelyTotal})
-          return previouslyReturned
+          arrayToReturn.push({customer: currentCart.customer, total: lovelyTotal})
+          return arrayToReturn
         }
         , []//initial value
       )    
